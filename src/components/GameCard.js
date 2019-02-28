409f266f55b60de904882e4c6105a1866c473e45
Vladyslav Vaskov <vaskov.vlad@gmail.com>
2019-02-28 15:08:14 +0200

    Add import components GameCard.js

---
+++ src/components/GameCard.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const GameCard = () => (
+    <div>
+        <h1>Example</h1>
+        <p>Project</p>
+    </div>
+);
+
+export default GameCard;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {render} from "react-dom";
 import 'semantic-ui-css/semantic.min.css';
+import GameCard from "./components/GameCard.js";
 
-render(<h1>Hi World</h1>, document.getElementById("root"));
+render(<GameCard/>, document.getElementById("root"));
