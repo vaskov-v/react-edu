bbbc5e9f5f38c9e85df247c5e7f7e81d23304ff2
Vladyslav Vaskov <vaskov.vlad@gmail.com>
2019-03-18 14:37:50 +0200

    Added user login

--- src/api.js
+++ src/api.js
@@ -9,6 +9,7 @@ export default {
     delete: game => axios.delete(`/api/unsafegames/${game._id}`)
   },
   users: {
-    create: user => axios.post(`/api/users`, {user})
+    create: user => axios.post("/api/users", {user}),
+    login: credentials => axios.post("/api/auth", {credentials}).then(res => res.data.token)
   }
 };
--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, {Component} from "react";
 import {Route} from "react-router-dom";
+import axios from "axios";
 import HomePage from "./HomePage";
 import GameNavigation from "./GameNavigation";
 import GamesPage from "./GamesPage";
 import ShowGamePage from "./ShowGamePage";
 import SignUpPage from "./SignUpPage";
+import LoginPage from "./LoginPage";
+
+const setAuthorizationHeader = (token = null) => {
+  if(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  } else {
+    delete axios.defaults.headers.common.Authorization;
+  }
+}
 
 class App extends Component {
   state = {
@@ -14,9 +24,24 @@ class App extends Component {
     message: ""
   };
 
-  setMessage = message => this.setState({message});
+  componentDidMount() {
+    if(localStorage.bgshopToken){
+      this.setState({user: {token: localStorage.bgshopToken}});
+      setAuthorizationHeader(localStorage.bgshopToken);
+    }
+  }
 
-  logout = () => this.setState({user: {token: null}});
+  setMessage = message => this.setState({message});
+  logout = () => {
+    this.setState({user: {token: null}});
+    setAuthorizationHeader();
+    localStorage.removeItem("bgshopToken")
+  }
+  login = token => {
+    this.setState({user:{token}});
+    localStorage.bgshopToken = token;
+    setAuthorizationHeader(token);
+  };
 
   render() {
     return (
@@ -36,6 +61,7 @@ class App extends Component {
         <Route path="/games" component={GamesPage}/>
         <Route path="/game/:_id" exact component={ShowGamePage}/>
         <Route path="/signup" render={props => (<SignUpPage {...props} setMessage={this.setMessage}/>)}/>
+        <Route path="/login" render={props => <LoginPage {...props} login={this.login}/>}/>
       </div>
     );
   }
